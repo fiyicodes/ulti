141fdcf82a94792ebcf2f5af438a94de3c34a20e
Onafeso Fiyifoluwa <fiyionafeso@gmail.com>
2019-08-05 13:03:11 +0100

    Add existing file

--- js/main.js
+++ js/main.js
@@ -2506,13 +2506,7 @@ $(document).ready(function () {
     /*==============================================================*/
     //demo button  - START CODE
     /*==============================================================*/
-    var $buythemediv = '<div class="buy-theme alt-font md-display-none"><a href="https://themeforest.net/item/pofo-creative-agency-corporate-and-portfolio-multipurpose-template/20645944?ref=themezaa" target="_blank"><i class="ti-shopping-cart"></i><span>Buy Theme</span></a></div><div class="all-demo alt-font md-display-none"><a href="mailto:info@themezaa.com?subject=POFO - Creative Agency, Corporate and Portfolio Multi-purpose Template - Quick Question"><i class="ti-email"></i><span>Quick Question?</span></a></div>';
-    $('body').append($buythemediv);
-
-    $(document).on("touchstart", ".sidebar-wrapper", function () {
-        clearOpen();
-    });
-
+   
     var getNav = $("nav.navbar.bootsnav"), getIn = getNav.find("ul.nav").data("in"), getOut = getNav.find("ul.nav").data("out");
     function clearOpen() {
         $('li.dropdown').removeClass("on").removeClass("show");
